270175507b836daab025fd1c4d0f1f5e24d8b3aa
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-13 17:51:47 -0300

    Add movies edit/update routes and form partial component.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,4 +13,21 @@ class MoviesController < ApplicationController
     end
     render :new
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    
+    if @movie.update(title: params[:movie][:title], released_at: params[:movie][:released_at], 
+      synopsis: params[:movie][:synopsis], country: params[:movie][:country], 
+      duration: params[:movie][:duration], director_id: params[:movie][:director_id], genre_id: params[:movie][:genre_id])
+
+      return redirect_to root_path
+    end
+
+    render :edit
+  end
 end
