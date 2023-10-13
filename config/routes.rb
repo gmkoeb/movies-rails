f2338b9e561d5438b569ebab3974d2190f49d069
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-13 16:56:25 -0300

    Fix routes GET path to adress route ambiguity.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/directors', to: 'directors#show'
   get '/genres', to: 'genres#show'
-  
-  get '/genres/show/:genre', to: 'genres#genre'
-  get '/directors/show/:director', to: 'directors#director'
+
+  get '/genres/details/:genre', to: 'genres#genre'
+  get '/directors/details/:director', to: 'directors#director'
+
   resources :genres, only: [:new, :create]
   resources :movies, only: [:new, :create]
   resources :directors, only: [:show, :new, :create]
