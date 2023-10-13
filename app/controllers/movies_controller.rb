0a5d5237ca8ced6cc0158763dec915e91fc948f4
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-13 12:09:12 -0300

    Add movie creation.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,16 @@
 class MoviesController < ApplicationController
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(title: params[:movie][:title], released_at: params[:movie][:released_at], 
+                      synopsis: params[:movie][:synopsis], country: params[:movie][:country], 
+                      duration: params[:movie][:duration], director: params[:movie][:director], genre_id: params[:movie][:genre_id])
+
+    if @movie.save
+      return redirect_to root_path
+    end
+    render :new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :genres, only: [:new, :create]
+  resources :movies, only: [:show, :new, :create]
 end
