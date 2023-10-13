37602b8391af14b20f7d6aeb78deae9b35b55d8b
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-13 10:28:55 -0300

    Add genre and movie one to many relationship.
    Remove genre column from movies table.

---
+++ db/migrate/20231013132644_remove_genre_column_from_movies.rb
@@ -0,0 +1,5 @@
+class RemoveGenreColumnFromMovies < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :movies, :genre, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_13_124929) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_132644) do
   create_table "directors", force: :cascade do |t|
     t.string "name"
     t.string "nationality"
@@ -33,9 +33,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_124929) do
     t.string "country"
     t.integer "duration"
     t.string "director"
-    t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genres_id", default: 0, null: false
+    t.index ["genres_id"], name: "index_movies_on_genres_id"
   end
 
+  add_foreign_key "movies", "genres", column: "genres_id"
 end
