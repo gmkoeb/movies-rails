defe9b257ed93ad579505645ecbfb4667beabbd1
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-10-13 10:17:25 -0300

    Include method to create new genres in genres controller.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,13 @@
 class GenresController < ApplicationController
+  def new
+  end
+  def create
+    new_genre = Genre.new(name: params[:genre][:name])
+
+    if new_genre.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
